5b9c9bc184d41c05a4b46d6981745af343c36061
mahmutoz <mahmutoz8197@gmail.com>
2021-11-19 01:08:05 +0300

    Tailwindcss add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import { MainProvider } from './context/MainContext';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainProvider>
+      <div className="p-2 bg-indigo-500">Deneme</div>
+    </MainProvider>
   );
 }
 
